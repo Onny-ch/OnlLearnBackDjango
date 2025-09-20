a27f17909337e9d9726b4e0dda8ade68e27f637f
Onny-ch <bezy4uy@gmail.com>
2025-09-21 00:42:14 +0300

    add Payments model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None
@@ -38,3 +40,47 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payments(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Имя пользователя",
+        help_text="Укажите отправителя платежа",
+    )
+    payment_date = models.DateField(
+        auto_now_add=True,
+        verbose_name="Дата платежа",
+    )
+    paid_course = models.ForeignKey(
+        Course,
+        null=True,
+        blank=True,
+        verbose_name="Оплаченный курс или урок",
+        help_text="Укажите оплаченный курс или урок",
+        on_delete=models.SET_NULL
+    )
+    paid_lesson = models.ForeignKey(
+        Lesson,
+        null=True,
+        blank=True,
+        verbose_name="Оплаченный курс или урок",
+        help_text="Укажите оплаченный курс или урок",
+        on_delete=models.SET_NULL
+    )
+    payment_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Оплата",
+        help_text="Укажите оплату",
+    )
+    payment_method = models.CharField(
+        max_length=50,
+        verbose_name="Метод оплаты",
+        help_text="Укажите метод оплаты",
+    )
+
+    class Meta:
+        verbose_name = "Оплата"
+        verbose_name_plural = "Оплаты"
