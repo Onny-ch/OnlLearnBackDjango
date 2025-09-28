018d7b6295baa52321c4c551311d2f694ce9efe0
Onny-ch <bezy4uy@gmail.com>
2025-09-28 13:57:20 +0300

    added permissions

--- users/views.py
+++ users/views.py
@@ -1,11 +1,48 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, filters
+from rest_framework import filters, generics, viewsets
+from rest_framework.permissions import AllowAny
 
-from users.models import Payments
+from users.models import Payments, User
+from users.serializers import UserSerializer
 
 
 class PaymentsViewSet(viewsets.ModelViewSet):
     queryset = Payments.objects.all()
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    filterset_fields = ["paid_course", "paid_lesson", "payment_method",]
-    ordering_fields = ["payment_date",]
+    filterset_fields = [
+        "paid_course",
+        "paid_lesson",
+        "payment_method",
+    ]
+    ordering_fields = [
+        "payment_date",
+    ]
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
