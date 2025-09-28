50829ad004cb62866cfaf635c624e2fdeea39032
Onny-ch <bezy4uy@gmail.com>
2025-09-28 13:58:25 +0300

    black, isort changes

--- users/management/commands/uu.py
+++ users/management/commands/uu.py
@@ -6,7 +6,7 @@ from users.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(email="notadmin@example.ru")
-        user.is_active =True
+        user.is_active = True
         user.is_staff = False
         user.is_superuser = False
         user.set_password("123qwe")
