51d1f65f582d7c92b5180c52afe9c355df1ad0a2
Onny-ch <bezy4uy@gmail.com>
2025-09-28 13:58:53 +0300

    add permissions check

--- materials/urls.py
+++ materials/urls.py
@@ -14,12 +14,14 @@ router.register("", CourseViewSet)
 urlpatterns = [
     path("lessons/", LessonListAPIView.as_view(), name="lessons_list"),
     path("lessons/create/", LessonCreateAPIView.as_view(), name="lessons_create"),
-    path("lessons/<int:pk>", LessonRetrieveAPIView.as_view(), name="lessons_retrieve"),
+    path("lessons/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lessons_retrieve"),
     path(
-        "lessons/update/<int:pk>", LessonUpdateAPIView.as_view(), name="lessons_update"
+        "lessons/update/<int:pk>/", LessonUpdateAPIView.as_view(), name="lessons_update"
     ),
     path(
-        "lessons/delete/<int:pk>", LessonDestroyAPIView.as_view(), name="lessons_destroy"
+        "lessons/delete/<int:pk>/",
+        LessonDestroyAPIView.as_view(),
+        name="lessons_destroy",
     ),
 ]  # + router.urls
 
