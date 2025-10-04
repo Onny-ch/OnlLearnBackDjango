fbf17a287966fd7089a045bad433289fc4dceb09
Onny-ch <bezy4uy@gmail.com>
2025-10-04 12:15:54 +0300

    homework corrections

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
 from materials.models import Course, Lesson
-from materials.validators import validate_permitted_words
+from materials.validators import ValidatePermittedWords
 from users.models import Subscription
 
 
@@ -13,11 +13,12 @@ class CourseSerializer(serializers.ModelSerializer):
 
 
 class LessonSerializer(serializers.ModelSerializer):
-    video_url = serializers.URLField(validators=[validate_permitted_words], required=False)
+    video_url = serializers.URLField(required=False)
 
     class Meta:
         model = Lesson
         fields = "__all__"
+        validators = [ValidatePermittedWords(field="video_url")]
 
 
 class CourseDetailSerializer(serializers.ModelSerializer):
--- materials/validators.py
+++ materials/validators.py
@@ -1,15 +1,22 @@
 from rest_framework.serializers import ValidationError
 
-permitted_words = ["youtube.com",]
+permitted_words = [
+    "youtube.com",
+]
 
 
-def validate_permitted_words(value):
-    counter = 0
+class ValidatePermittedWords:
+    def __init__(self, field):
+        self.field = field
 
-    for address in permitted_words:
-        if address not in value:
-            counter += 1
-    if counter > 0:
-        raise ValidationError(
-            f"Ссылка должна быть правильного формата на данные видеохостинги: {', '.join(permitted_words)}"
-        )
+    def __call__(self, url):
+        counter = 0
+
+        for address in permitted_words:
+            if address not in url["video_url"]:
+                counter += 1
+
+        if counter > 0:
+            raise ValidationError(
+                f"Ссылка должна быть правильного формата на данные видеохостинги: {', '.join(permitted_words)}"
+            )
