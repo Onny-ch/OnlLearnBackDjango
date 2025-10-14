dc76a655d487e2598cf28c2a3f3c297ef056cf87
Onny-ch <bezy4uy@gmail.com>
2025-10-14 18:10:55 +0300

    le finale fixes

--- materials/tasks.py
+++ materials/tasks.py
@@ -4,11 +4,10 @@ from django.core.mail import send_mail
 
 
 @shared_task
-def sending_a_course_update_email(email):
+def sending_a_course_update_email(emails):
     send_mail(
         "Обновление курса2.",
         "Курс, на который вы подписаны, обновлен! Проверьте обновление.",
         settings.EMAIL_HOST_USER,
-        [email],
+        emails,
     )
-    print("All ok!")
