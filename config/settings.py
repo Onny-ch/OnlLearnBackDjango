ddc94aa7ce88b3afd6d0da1d64444de2b75d31ac
Onny-ch <bezy4uy@gmail.com>
2025-10-22 14:48:21 +0300

    changes in settings and add linter settings

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+import sys
 from datetime import timedelta
 # from datetime import timedelta
 from pathlib import Path
@@ -192,3 +193,11 @@ CELERY_BEAT_SCHEDULE = {
         "schedule": timedelta(days=1),
     },
 }
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'test_db.sqlite3',
+        }
+    }
