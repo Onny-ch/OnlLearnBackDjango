644359af4465226a0fe4ef5d90141ea53ce981e5
Onny-ch <bezy4uy@gmail.com>
2025-10-22 22:44:12 +0300

    ci.yml for tests #5.9

--- test_settings.py
+++ test_settings.py
@@ -2,6 +2,10 @@ from config.settings import *
 
 SECRET_KEY = 'test-secret-key-for-ci-only'
 
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_TASK_EAGER_PROPAGATES = True
+CELERY_BROKER_URL = 'memory://'
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
